ea4715edff77b11465dd8add1958522b407503b7
Isac Vidal <isac.a.vidal.leite@avanade.com>
2023-03-28 10:39:37 -0300

    Projeto React- Final

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { Post } from "./components/Post"
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css'
+
+import './global.css';
+
+const posts = [
+  {
+    id:1,
+    author: {
+      avatarUrl: "https://github.com/IsacVidal.png",
+      name: "Isac Vidal",
+      role: "Web Developer",
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id:2,
+    author: {
+      avatarUrl: "https://github.com/IsacVidal.png",
+      name: "Mariana Vidal",
+      role: "Analista de Comunicação",
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galera'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-11-20 20:00:00'),
+  }
+]
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+
